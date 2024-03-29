4657c817a1b842fa8c8aa213d10cc828305f8f88
НафНафДурикович <yana88yana99@gmail.com>
2024-03-30 00:12:30 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ public class Main {
         byte appleInBasket = 36;
         short thousandths = 19900;
         int million = 800000000;
-        long quintillionth = 900000000676767898l;
-        float justPoint = 4.67869968796784567457678787867f;
-        double bigPoint = 78567.4353564576575673456364756756857867896356456778967867878d;
+        long million2 = 900000000;
+        float justPoint = 4.678699f;
+        double bigPoint = 78567.4353564576;
+        System.out.println("Значение переменной яболок в коробке с типом byte равно " + appleInBasket);
+        System.out.println("Значение переменной тысячных долей с типом short равно " + thousandths);
+        System.out.println("Значение переменной милиона с типом int равно " + million);
+        System.out.println("Значение переменной миллин 2 с типом long равно " + million2);
+        System.out.println("Значение переменной просто точка с типом float равно " + justPoint);
+        System.out.println("Значение переменной большая точка с типом double равно " + bigPoint);
     }
 }
