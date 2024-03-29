20e4cc85e5fff887c70e0a1878b491d8a8df065e
НафНафДурикович <yana88yana99@gmail.com>
2024-03-29 23:51:17 +0300

    Пытаюсь понять первую задачу2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         byte appleInBasket = 36;
-        short thousandths = 1990;
+        short thousandths = 19900;
         int million = 800000000;
         long quintillionth = 900000000676767898l;
         float justPoint = 4.67869968796784567457678787867f;
+        double bigPoint = 78567.4353564576575673456364756756857867896356456778967867878d;
     }
 }
