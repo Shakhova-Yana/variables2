848f8ddf5051b9d457f15850b0a8f2073c5f7f3f
НафНафДурикович <yana88yana99@gmail.com>
2024-03-29 23:46:36 +0300

    Пытаюсь понять первую задачу

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        byte appleInBasket = 36;
+        short thousandths = 1990;
+        int million = 800000000;
+        long quintillionth = 900000000676767898l;
+        float justPoint = 4.67869968796784567457678787867f;
+    }
+}
